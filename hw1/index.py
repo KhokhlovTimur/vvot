d56f4351c380e15cd41dea7aaaaafa5289a10dbf
Pavlov Toni <t.kh25@mail.ru>
2025-01-18 15:22:32 +0300

    init: добавил функционал до создания бакета

---
+++ hw1/index.py
@@ -0,0 +1,2 @@
+def handler():
+    print(123)
---
+++ hw1/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
+provider "yandex" {
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id
+  service_account_key_file = "../../.yc-keys/key.json"
+}
+
+resource "yandex_iam_service_account" "bot_acc" {
+  name        = "func-bot-account"
+  description = "Аккаунт для функции с ботом"
+  folder_id   = var.folder_id
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "mount-iam" {
+  folder_id = var.folder_id
+  role      = "storage.admin"
+
+  members = [
+    "serviceAccount:${yandex_iam_service_account.bot_acc.id}",
+  ]
+}
+
+resource "yandex_storage_bucket" "mount-bucket" {
+  bucket = "khoklovte-ocr-bot-mount"
+  folder_id = var.folder_id
+}
+
+
+variable "TG_API_KEY" {
+  type = string
+  description = "Ключ тг бота"
+}
+
+variable "cloud_id" {
+  type = string
+  description = "ID облака"
+}
+
+variable "folder_id" {
+  type = string
+  description = "ID каталога"
+}
