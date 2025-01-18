8caafec7ef8d57a622c7ee171f9a489098d02100
KhokhlovTimur <t.kh25@mail.ru>
2025-01-18 18:00:27 +0300

    feat: дописал main.tf

--- hw1/main.tf
+++ hw1/main.tf
@@ -7,60 +7,82 @@ terraform {
   required_version = ">= 0.13"
 }
 
-
 provider "yandex" {
   cloud_id = var.cloud_id
   folder_id = var.folder_id
-  service_account_key_file = "../../.yc-keys/key.json"
-}
-
-resource "yandex_iam_service_account" "bot_acc" {
-  name        = "func-bot-account"
-  description = "Аккаунт для функции с ботом"
-  folder_id   = var.folder_id
-}
-
-resource "yandex_resourcemanager_folder_iam_binding" "mount-iam" {
-  folder_id = var.folder_id
-  role      = "storage.admin"
-
-  members = [
-    "serviceAccount:${yandex_iam_service_account.bot_acc.id}",
-  ]
+  service_account_key_file = "../../../terraform/.yc-keys/key.json"
 }
 
 resource "yandex_storage_bucket" "mount-bucket" {
-  bucket = "khoklovte-ocr-bot-mount"
+  bucket = "khokhlovte-ocr-bot-mount"
   folder_id = var.folder_id
 }
 
 
-resource "yandex_resourcemanager_folder_iam_binding" "ocr-iam" {
-  folder_id = var.folder_id
-  role      = "ai.vision.user"
+resource "yandex_function" "handler_func" {
+  name        = "func-bot"
+  user_hash   = archive_file.zip.output_sha256
+  runtime     = "python312"
+  entrypoint  = "index.handler"
+  memory      = 128
+  execution_timeout  = 20
+  environment = {
+    "TG_API_KEY" = var.TG_API_KEY,
+    "IMAGES_BUCKET" = yandex_storage_bucket.mount-bucket.bucket
+  }
+
+  storage_mounts {
+    mount_point_name = "images"
+    bucket = yandex_storage_bucket.mount-bucket.bucket
+    prefix           = ""
+  }
+
+  content {
+    zip_filename = archive_file.zip.output_path
+  }
+}
+
+output "func_url" {
+  value = "https://functions.yandexcloud.net/${yandex_function.handler_func.id}"
+}
+
+resource "archive_file" "zip" {
+  type = "zip"
+  output_path = "src.zip"
+  source_file = "./handler/index.py"
+}
+
+
+resource "yandex_storage_object" "yagpt_setup" {
+  bucket = yandex_storage_bucket.mount-bucket.id
+  key    = "instruction.txt"
+  source = "./instruction.txt"
+}
+
+resource "yandex_function_iam_binding" "function-iam" {
+  function_id = yandex_function.handler_func.id
+  role        = "serverless.functions.invoker"
 
   members = [
-    "serviceAccount:${yandex_iam_service_account.bot_acc.id}",
+    "system:allUsers",
   ]
 }
 
-resource "yandex_resourcemanager_folder_iam_binding" "yagpt-iam" {
-  folder_id = var.folder_id
-  role      = "ai.languageModels.user"
 
-  members = [
-    "serviceAccount:${yandex_iam_service_account.bot_acc.id}",
-  ]
-}
-
-resource "yandex_resourcemanager_folder_iam_binding" "func-admin-iam" {
-  folder_id = var.folder_id
-  role      = "serverless.functions.admin"
-
-  members = [
-    "serviceAccount:${yandex_iam_service_account.bot_acc.id}",
-  ]
-}
+# resource "null_resource" "triggers" {
+#   triggers = {
+#     api_key = var.TG_API_KEY
+#   }
+#
+#   provisioner "local-exec" {
+#     command = "curl --insecure -X POST https://api.telegram.org/bot${var.TG_API_KEY}/setWebhook?url=https://functions.yandexcloud.net/${yandex_function.handler_func.id}"
+#   }
+#
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = "curl --insecure -X POST https://api.telegram.org/bot${self.triggers.api_key}/deleteWebhook"
+#   }
+# }
 
 
 variable "TG_API_KEY" {
