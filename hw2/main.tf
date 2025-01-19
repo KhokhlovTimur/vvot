141f4785cd7f90b4fcebf79c055611cffa64d7b2
KhokhlovTimur <t.kh25@mail.ru>
2025-01-19 13:36:16 +0300

    feat: добавил пустой обработчик лиц

--- hw2/main.tf
+++ hw2/main.tf
@@ -13,21 +13,12 @@ provider "yandex" {
   service_account_key_file = "../../../terraform/.yc-keys/key.json"
 }
 
-
 resource "yandex_iam_service_account" "func-bot-account-kte-faces" {
   name        = "func-bot-account-kte-faces"
   description = "Аккаунт для взаимодействия"
   folder_id   = var.folder_id
 }
 
-resource "yandex_resourcemanager_folder_iam_binding" "mount-iam" {
-  folder_id = var.folder_id
-  role      = "storage.admin"
-
-  members = [
-    "serviceAccount:${yandex_iam_service_account.func-bot-account-kte-faces.id}",
-  ]
-}
 
 resource "yandex_resourcemanager_folder_iam_binding" "ocr-iam" {
   folder_id = var.folder_id
@@ -38,16 +29,44 @@ resource "yandex_resourcemanager_folder_iam_binding" "ocr-iam" {
   ]
 }
 
-
-resource "yandex_storage_bucket" "mount-bucket" {
-  bucket    = "khokhlovte-ocr-bot-mount"
+resource "yandex_storage_bucket" "bucket-photos" {
+  bucket    = "vvot16-photos"
   folder_id = var.folder_id
 }
 
-resource "archive_file" "zip" {
+resource "yandex_storage_bucket" "bucket-faces" {
+  bucket    = "vvot16-faces"
+  folder_id = var.folder_id
+}
+
+resource "yandex_function" "face-detection-handler" {
+  name              = "vvot16-face-detection"
+  user_hash         = archive_file.face-detection-handler-zip.output_base64sha256
+  runtime           = "python312"
+  entrypoint        = "face-detection-handler.handler"
+  memory            = 128
+  execution_timeout = 20
+
+  environment = {
+  }
+
+  service_account_id = yandex_iam_service_account.func-bot-account-kte-faces.id
+
+  storage_mounts {
+    mount_point_name = "images"
+    bucket           = yandex_storage_bucket.bucket-photos
+    prefix           = ""
+  }
+
+  content {
+    zip_filename = archive_file.face-detection-handler-zip.output_path
+  }
+}
+
+resource "archive_file" "face-detection-handler-zip" {
   type        = "zip"
-  output_path = "src.zip"
-  source_file = "./handler/index.py"
+  output_path = "face-detection-handler.zip"
+  source_file = "./handlers/face-detection-handler.py"
 }
 
 resource "null_resource" "triggers" {
