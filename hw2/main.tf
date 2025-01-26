3d8761ef50348915ffb87f1cc8443d7b0e5157ff
KhokhlovTimur <t.kh25@mail.ru>
2025-01-26 12:14:58 +0300

    fix: поменял путь

--- hw2/main.tf
+++ hw2/main.tf
@@ -7,10 +7,14 @@ terraform {
   required_version = ">= 0.13"
 }
 
+locals {
+  key_dir = "../../../terraform"
+}
+
 provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
-  service_account_key_file = "../../../terraform/.yc-keys/key.json"
+  service_account_key_file = "${local.key_dir}/.yc-keys/key.json"
 }
 
 resource "yandex_iam_service_account" "func-bot-account-kte-faces" {
